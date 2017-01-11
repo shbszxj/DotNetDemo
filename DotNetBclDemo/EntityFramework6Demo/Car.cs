4ed3d1e44ad37716d86ae28e81623903d376b513
Zhou, Xujun <xu-jun.zhou@outlook.com>
2017-01-11 13:24:47 +0800

    add demo

---
+++ DotNetBclDemo/EntityFramework6Demo/Car.cs
@@ -0,0 +1,8 @@
+﻿public class Car
+{
+    public int Id { get; set; }
+
+    public string Number { get; set; }
+
+    public string Band { get; set; }
+}
---
+++ WcfTestService/WcfServiceLib/Service1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+
+namespace WcfServiceLib
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class Service1 : IHelloWorld
+    {
+        private int messageCount;
+        private Dictionary<int, string> dic = new Dictionary<int, string>();
+
+        public void HelloWorld()
+        {
+            dic.Add(messageCount, "Hello,World");
+
+            Console.WriteLine("序列号{0},线程号{1}", messageCount, Thread.CurrentThread.ManagedThreadId);
+            messageCount++;
+
+            Thread.Sleep(50);
+        }
+    }
+}
