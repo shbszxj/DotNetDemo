3af2d119507d407c13713fd0d1515b46d1a23ce7
Jack Zhou <Jack.Zhou@televic.com>
2018-07-26 09:26:37 +0800

    add demo for ReferenceEquals()

---
+++ DotNetBclDemo/EqualsDemo/ValueReferenceEqualsExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualsDemo
+{
+    public class ValueReferenceEqualsExample
+    {
+        public void Example()
+        {
+            int i = 5;
+            int j = 5;
+
+            if (ReferenceEquals(i, j))
+                Console.WriteLine("Never happens.");
+            else
+                Console.WriteLine("Always happens.");
+
+            if (ReferenceEquals(i, i))
+                Console.WriteLine("Never happens.");
+            else
+                Console.WriteLine("Always happens.");
+        }
+    }
+}
