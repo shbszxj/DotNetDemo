ca9a0b35e83194146b3ed9616df10ecf849a0bbd
Jack Zhou <Jack.Zhou@televic.com>
2018-07-30 16:57:20 +0800

    example of IEqualityComparer

--- DotNetBclDemo/EqualsDemo/Program.cs
+++ DotNetBclDemo/EqualsDemo/Program.cs
@@ -13,12 +13,22 @@ namespace EqualsDemo
             // value type reference euqals example
             new ValueTypeReferenceEqualsExample().Example();
             Console.WriteLine("==========================================");
+
             // IEquatable example
             var janet = new Person { Id = 1, FirstName = "Janet", LastName = "Jackson" };
             var michael = new Person{ Id = 2, FirstName = "Michael", LastName = "Jackson" };
             var tom = new Person() {Id = 1, FirstName = "Tom", LastName = "Stephen"};
             Console.WriteLine($"Janet equals Michael {janet.Equals(michael)}");
             Console.WriteLine($"Janet equals Tom {janet.Equals(tom)}");
+            Console.WriteLine("==========================================");
+
+
+            var t1 = Tuple.Create<int, string>(1, "Stephanie");
+            var t2 = Tuple.Create<int, string>(1, "Stephanie");
+            if (t1 != t2)
+                Console.WriteLine("not the same reference to the tuple.");
+            if (t1.Equals(t2))
+                Console.WriteLine("the same content.");
 
             Console.Read();
         }
---
+++ DotNetBclDemo/EqualsDemo/TupleComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualsDemo
+{
+    public class TupleComparer<T1, T2> : IEqualityComparer
+    {
+        public new bool Equals(object x, object y)
+        {
+            return x.Equals(y);
+        }
+
+        public int GetHashCode(object obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
