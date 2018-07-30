f64890113d8019f33bf0f7d97051dec201db4703
Jack Zhou <Jack.Zhou@televic.com>
2018-07-30 16:47:37 +0800

    example of IEquatable

---
+++ DotNetBclDemo/EqualsDemo/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualsDemo
+{
+    public class Person : IEquatable<Person>
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} {FirstName} {LastName}";
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public bool Equals(Person other)
+        {
+            if (other == null)
+                return base.Equals(other);
+
+            return Id == other.Id;
+        }
+    }
+}
--- DotNetBclDemo/EqualsDemo/Program.cs
+++ DotNetBclDemo/EqualsDemo/Program.cs
@@ -10,7 +10,16 @@ namespace EqualsDemo
     {
         static void Main(string[] args)
         {
+            // value type reference euqals example
             new ValueTypeReferenceEqualsExample().Example();
+            Console.WriteLine("==========================================");
+            // IEquatable example
+            var janet = new Person { Id = 1, FirstName = "Janet", LastName = "Jackson" };
+            var michael = new Person{ Id = 2, FirstName = "Michael", LastName = "Jackson" };
+            var tom = new Person() {Id = 1, FirstName = "Tom", LastName = "Stephen"};
+            Console.WriteLine($"Janet equals Michael {janet.Equals(michael)}");
+            Console.WriteLine($"Janet equals Tom {janet.Equals(tom)}");
+
             Console.Read();
         }
     }
