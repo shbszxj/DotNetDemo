8dce6215c30b4c08f47ea3d2b9a358dc1c287fd8
Jack Zhou <Jack.Zhou@televic.com>
2018-08-21 09:28:50 +0800

    add example3

--- DotNetBclDemo/QuerySyntax/ExampleThree.cs
+++ DotNetBclDemo/QuerySyntax/ExampleThree.cs
@@ -6,18 +6,28 @@ using System.Threading.Tasks;
 
 namespace QuerySyntax
 {
-    public class ExampleTwo
+    public class ExampleThree
     {
         public static IEnumerable<Tuple<int, int>> ProduceIndices()
         {
+            var storage = new List<Tuple<int, int>>();
+
             for (int x = 0; x < 100; x++)
             {
                 for (int y = 0; y < 100; y++)
                 {
                     if (x + y < 100)
-                        yield return Tuple.Create(x, y);
+                    {
+                        storage.Add(Tuple.Create(x, y));
+                    }
                 }
             }
+
+            storage.Sort((point1, point2) =>
+                (point2.Item1 * point2.Item1 + point2.Item2 * point2.Item2)
+                    .CompareTo(point1.Item1 * point1.Item1 + point1.Item2 * point1.Item2));
+
+            return storage;
         }
 
         public static IEnumerable<Tuple<int, int>> QueryIndices()
@@ -25,6 +35,7 @@ namespace QuerySyntax
             return from x in Enumerable.Range(0, 100)
                    from y in Enumerable.Range(0, 100)
                    where x + y < 100
+                   orderby (x * x + y * y) descending
                    select Tuple.Create(x, y);
         }
     }
