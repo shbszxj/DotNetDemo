faadd3563fb2d83f4cf4abdc313cd169ec1dbc8a
Jack Zhou <J.Zhou@televic.com>
2019-04-09 15:18:09 +0800

    add demo of simple factory

---
+++ DotNetBclDemo/SimpleFactory.Demo/Operation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory.Demo
+{
+    public class Operation
+    {
+        public double NumberA { get; set; }
+
+        public double NumberB { get; set; }
+
+        public virtual double GetResult()
+        {
+            return 0;
+        }
+    }
+}
---
+++ DotNetBclDemo/SimpleFactory.Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var operation = OperationFactory.CreateOperation("+");
+            operation.NumberA = 10;
+            operation.NumberB = 2;
+            Console.WriteLine(operation.GetResult());
+            Console.Read();
+        }
+    }
+}
